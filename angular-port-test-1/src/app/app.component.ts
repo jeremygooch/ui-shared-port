93fbaf4a5e9b9cabd651552b47060e45b427aa44
Jeremy Gooch <jeremy.gooch@gmail.com>
2023-07-29 09:07:05 -0500

    Adding session and local storage setting and retrieval in angular1 project

--- angular-port-test-1/src/app/app.component.ts
+++ angular-port-test-1/src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-port-test-1';
+  sessionStorage = sessionStorage;
+  localStorage = localStorage;
+
+  sessionStorageKey = 'my-session-data';
+  localStorageKey = 'my-local-data';
+
+  private readonly sensitiveSessionData = {
+    userId: '123-456-789',
+    otherData: [1,2,3,4,5]
+  };
+
+  private readonly sensitiveLocalData = {
+    userId: '987-654-321',
+    otherData: [5,4,3,2,1]
+  };
+
+  constructor() {
+    this.sessionStorage.setItem(this.sessionStorageKey, JSON.stringify(this.sensitiveSessionData));
+    this.localStorage.setItem(this.localStorageKey, JSON.stringify(this.sensitiveLocalData));
+  }
 }
